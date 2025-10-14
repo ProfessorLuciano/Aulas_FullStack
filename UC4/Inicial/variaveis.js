3ac582ff725dd6db3b4324e4517a405e770c86b4
ProfLuciano <luciano.silva@sp.senac.br>
2025-10-14 17:34:33 -0300

    14-10

---
+++ UC4/Inicial/variaveis.js
@@ -0,0 +1,8 @@
+let nome = 'Luciano Custodio' //Criando uma variavel TEXTO
+let numero = 10 //Criando uma variavel NUMERICA
+let situacao = false //Criando uma variavel BOOLEANA
+console.log(situacao) //Mostrar saida na TELA
+
+/*o typeof é utilizado para mostrar 
+o tipo de dado armazenado na variavel
+exemplo console.log(typeof situacao) */
---
+++ UC4/resolverProblema.js
@@ -0,0 +1,22 @@
+/*Exercício 1 – Multiplicação com Situação Problema
+Enunciado:
+Joana comprou 4 cadernos. Cada caderno custou R$ 12,00.
+Pergunta:
+Quanto ela gastou no total? */
+
+let cadernos = 4
+let custo = 12.56
+let resultado = cadernos + custo
+//console.log('Resposta: Joana gastou R$: ' + resultado + ' em cadernos')
+
+
+/*1.	Calculadora de IMC
+Crie um algoritmo que receba o peso e a altura de uma pessoa e 
+calcule o Índice de Massa Corporal (IMC).
+Formula IMC = Peso (kg) / Altura (m) x Altura (m) 
+*/
+
+let peso = 87
+let altura = 1.71
+let resultado1 = peso / (altura * altura) 
+console.log('O IMC encontrado foi: ' + resultado1.toFixed(1))
