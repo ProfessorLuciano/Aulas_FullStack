2f01291b55d47a98b76766f3ad12cf5b32e672c3
ProfLuciano <luciano.silva@sp.senac.br>
2025-10-21 17:26:45 -0300

    21-10

---
+++ UC4/Inicial/if.js
@@ -0,0 +1,50 @@
+/*Comparação Simples
+if (4 > 3) {
+    console.log('É verdadeiro')
+} else {
+    console.log('É falso')
+}*/
+/*
+let n1 = 30
+const n2 = 2
+let resultado = n1 % n2
+if (resultado == 0) {
+    console.log('É Par')
+} else {
+    console.log('É Impar')
+}*/
+/*
+const maioridade = 18
+let idade = 17
+if( idade >= maioridade){
+    console.log('18 anos ou mais')
+}else{
+    console.log('Menos de 18 anos')
+}*/
+/*
+let temperatura = 26
+const t1 = 15
+const t2 = 25
+if (temperatura < t1) {
+    console.log('Está frio!!!!')
+} else if (temperatura >= t1 && temperatura <= t2) {
+    console.log('Está agradável!!!!')
+} else {
+    console.log('Está quente!!!!')
+}*/
+const notaCorte = 7
+let notaAluno = 9
+if(notaAluno >= notaCorte){
+    console.log('Está aprovado')
+}else{
+    console.log('Está reprovado')
+}
+
+
+
+
+
+
+
+
+
