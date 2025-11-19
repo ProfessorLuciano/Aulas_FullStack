9bb7b2063b15286e317d5fd5caa8f1049293904e
ProfLuciano <luciano.silva@sp.senac.br>
2025-11-19 14:39:25 -0300

    19-11

--- UC4/Frontend/front/src/App.jsx
+++ UC4/Frontend/front/src/App.jsx
@@ -8,6 +8,17 @@ export default function App() {
       console.log(resposta.data)
     }
 
+    async function cadastrarFuncionarios(){
+      const nome = 'Luciano Silva'
+      const cpf = '11111111113'
+      const email = 'lucianosilva@teste.com.br'
+      const senha = '123456'
+      const status = true
+      const idHierarquia = '7d8c0a1c-87c6-4a3a-adda-eca2ea221cd5'
+
+      const resposta = await apiLocal.post('/CadastrarFuncionarios', {})
+    }
+
   return (
     <>
       <div className='appGeral'>
