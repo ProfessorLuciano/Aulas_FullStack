a1a1bf111103ac59a0011e3c7e5819d98da03f67
ProfLuciano <luciano.silva@sp.senac.br>
2026-06-03 17:21:22 -0300

    03-06

---
+++ UC4/Algoritimos/operacao_matematica.js
@@ -0,0 +1,36 @@
+//Subtração
+/*1.Problema de Adição e Subtração:
+João tem 50 reais. Ele compra um brinquedo que custa 30 reais e depois compra um lanche que custa 10 reais. Quantos reais ele tem agora?
+/*var n1 = 50
+var n2 = 30
+var n3 = 10
+var subtracao = n1 - n2 - n3
+console.log(subtracao)*/
+
+//Multiplicação
+/*2.Problema de Multiplicação:
+Uma caixa contém 8 pacotes de figurinhas. Se cada pacote tem 12 figurinhas, quantas figurinhas há no total na caixa?*/
+/*var pacotes = 8
+var figurinhas = 12
+var total = pacotes * figurinhas
+console.log(total)*/
+
+//Divisão
+/*3.Problema de Divisão:
+Um agricultor tem 96 maçãs e quer dividi-las igualmente entre 8 cestas. Quantas maçãs vão para cada cesta?*/
+/*var macas = 96
+var cestas = 8
+var divisao = macas / cestas
+console.log(divisao)*/
+
+//Proporção
+/* 4.Problema de Proporção:
+Em uma escola, a proporção de alunos que jogam futebol para os que jogam basquete é 3 para 2. Se há 90 alunos que jogam futebol, quantos alunos jogam basquete?*/
+/*var alunosFutebol = 90
+var proporcaoFutebol = 3
+var proporcaoBasquete = 2
+var alunosBasquete = ((alunosFutebol * proporcaoBasquete) / proporcaoFutebol)
+console.log(alunosBasquete)*/
+
+
+
---
+++ UC4/Algoritimos/var_const.js
@@ -0,0 +1,5 @@
+//Declaração de variável
+var numero = 10.45
+//Imprime na tela uma variavel ou constante
+//typeof = palavra reservada que identifica e mostra o tipo de dado atribuido, syntax console.log(typeof numero)
+console.log(typeof numero)
