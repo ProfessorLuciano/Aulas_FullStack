25aba6464b40e00e0b636a858de7980e04e4be4a
ProfLuciano <luciano.silva@sp.senac.br>
2026-06-08 14:59:58 -0300

    08-06

---
+++ UC4/Algoritimos/funcoes.js
@@ -0,0 +1,12 @@
+//Função Sem Argumento
+function nome(){
+    var a = 2
+    var b = 3
+    console.log(a * b)
+}
+nome()
+//Função com Argumento
+function nome(a){
+    console.log(a)
+}
+nome1('Ola, Mundo')
--- UC4/Algoritimos/var_const.js
+++ UC4/Algoritimos/var_const.js
@@ -1,5 +1,21 @@
 //Declaração de variável
-var numero = 10.45
+//var numero = 10.45
 //Imprime na tela uma variavel ou constante
 //typeof = palavra reservada que identifica e mostra o tipo de dado atribuido, syntax console.log(typeof numero)
-console.log(typeof numero)
+//console.log(typeof numero)
+
+//var n1 = 10
+//n1 = 20
+//console.log(n1)
+
+const nome = 10
+const sobrenome = 'teste2'
+console.log(typeof String(nome))
+
+
+
+
+
+
+
+
