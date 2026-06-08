594227075de5763e5d5ba2d4a1c03b2591c275e8
ProfLuciano <luciano.silva@sp.senac.br>
2026-06-08 17:36:17 -0300

    08-06

---
+++ UC4/Algoritimos/exercicios.js
@@ -0,0 +1,23 @@
+//Calculo de IMC
+/*function calcularImc() {
+    let peso = 89
+    let altura = 1.70
+    let IMC = (peso / (altura ^ 2))
+    console.log(IMC.toFixed(2))
+}
+calcularImc()*/
+
+//Calculadora de Troco sem argumentos
+/*function calculadoraTroco(){
+    const troco = 0
+    let valorPago = 100
+    let valorCompra = 50
+    console.log(troco = valorPago - valorCompra)
+}
+calculadoraTroco()*/
+
+//Calculadora de Troco sem argumentos
+function calculadoraTroco(valorPago, valorCompra){
+    console.log(valorPago - valorCompra)
+}
+calculadoraTroco(100, 50)
