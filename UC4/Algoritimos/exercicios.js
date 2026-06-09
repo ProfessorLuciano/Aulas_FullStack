f5504c91057c9cd4673ad092e9df96fda5446799
ProfLuciano <luciano.silva@sp.senac.br>
2026-06-09 15:20:29 -0300

    09-06

--- UC4/Algoritimos/exercicios.js
+++ UC4/Algoritimos/exercicios.js
@@ -17,7 +17,41 @@ calcularImc()*/
 calculadoraTroco()*/
 
 //Calculadora de Troco sem argumentos
-function calculadoraTroco(valorPago, valorCompra){
+/*function calculadoraTroco(valorPago, valorCompra){
     console.log(valorPago - valorCompra)
 }
-calculadoraTroco(100, 50)
+calculadoraTroco(100, 50)*/
+
+//Calcule um numero e escreva na tela se é PAR ou IMPAR sem IF
+/*function parOuImpar() {
+    let n1 = 11
+    const divisor = 2
+    const resto = 0
+    let resultado = n1 % divisor
+    if (resultado === resto) {
+        console.log('PAR')
+    } else {
+        console.log('IMPAR')
+    }
+}
+parOuImpar()*/
+
+/*function parOuImpar() {
+    let n1 = 11
+    const divisor = 2
+    const resto = 0
+    let resultado = n1 % divisor
+    console.log(resultado === resto ? 'PAR' : 'IMPAR')
+}
+parOuImpar()*/
+
+function parOuImpar() {
+    console.log( '10' === 10 ? 'VERDADEIRO' : 'FALSO')
+}
+parOuImpar()
+
+
+
+
+
+
