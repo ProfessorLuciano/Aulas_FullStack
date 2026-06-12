5cbd76482d6b7640b8c5c9219f4737b16a9de5e7
ProfLuciano <luciano.silva@sp.senac.br>
2026-06-12 17:34:27 -0300

    12-06

---
+++ UC2/aulainicial/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.scss'
+import Logo from './assets/logo.png'
+
+export default function App() {
+
+
+  return (
+    <>
+      <div className='conteinerGeralApp'>
+        <h1>Site de Informática</h1>
+        <img src={Logo} alt="" />
+      </div>
+    </>
+  )
+}
