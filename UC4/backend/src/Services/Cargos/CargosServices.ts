641fe9b085cf1dfa75414023c99174e95383926b
ProfLuciano <luciano.silva@sp.senac.br>
2026-07-01 14:49:06 -0300

    01-07

--- UC4/backend/src/Services/Cargos/CargosServices.ts
+++ UC4/backend/src/Services/Cargos/CargosServices.ts
@@ -1,13 +1,17 @@
 import prismaClient from '../../Prisma/PrismaClient'
 
 class CargosServices {
-    async cadastrarCargos(nome: string){
-       const resposta = await prismaClient.cargos.create({
-        data: {
-            nome: nome
-        }
-       })
-       return resposta
+    async cadastrarCargos(nome: string) {
+        const resposta = await prismaClient.cargos.create({
+            data: {
+                nome: nome
+            },
+            select: {
+                id: true,
+                nome: true
+            }
+        })
+        return resposta
     }
 }
 
