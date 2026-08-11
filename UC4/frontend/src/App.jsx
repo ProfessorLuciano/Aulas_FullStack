d741cad86018902aa1b7934ad8ec2d48450f3898
ProfLuciano <luciano.silva@sp.senac.br>
2026-08-11 17:31:08 -0300

    11-08

--- UC4/frontend/src/App.jsx
+++ UC4/frontend/src/App.jsx
@@ -2,12 +2,23 @@
 import './App.scss'
 
 export default function App() {
+
+  const array = [{nome: 'luciano', telefone: '11111111'}]
   
 
   return (
     <>
       <div>
         <h1>Front com API</h1>
+
+        {array.map((item) => {
+          return(
+            <>
+          <p>{item.nome}</p>
+          <p>{item.telefone}</p>
+            </>
+          )
+        })}
         
       </div>
     </>
