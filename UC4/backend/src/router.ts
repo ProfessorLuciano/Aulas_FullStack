137c0634881ad18f08f05fa00c23e558eb029bcf
ProfLuciano <luciano.silva@sp.senac.br>
2026-09-02 16:32:38 -0300

    02-09

--- UC4/backend/src/router.ts
+++ UC4/backend/src/router.ts
@@ -9,25 +9,25 @@ import { ProdutosControllers } from './Controllers/Produtos/ProdutosControllers'
 import { LoginUsuariosControllers } from './Controllers/LoginUsuarios/LoginUsuariosControllers'
 
 const router = Router()
-
 const upload = multer(uploadConfig.upload('./tmp'))
+import { estaAutenticado } from './Middleware/estaAutenticado'
 
 //Criação dos EndPoints
 //Rotas de Usuarios
 router.post('/CadastrarUsuarios', new UsuariosControllers().cadastrarUsuarios)
-router.post('/VisualizarUsuarioUnicoPost', new UsuariosControllers().visualizarUsuarioUnicoPost)
-router.put('/AlterarUsuarios', new UsuariosControllers().alterarUsuarios)
-router.get('/VisualizarUsuarioUnicoGet/:id', new UsuariosControllers().visualizarUsuarioUnicoGet)
-router.get('/VisualizarDadosGeral', new UsuariosControllers().visualizarDadosGeral)
-router.delete('/ApagarUsuarios', new UsuariosControllers().apagarUsuarios)
+router.post('/VisualizarUsuarioUnicoPost', estaAutenticado, new UsuariosControllers().visualizarUsuarioUnicoPost)
+router.put('/AlterarUsuarios', estaAutenticado, new UsuariosControllers().alterarUsuarios)
+router.get('/VisualizarUsuarioUnicoGet/:id', estaAutenticado, new UsuariosControllers().visualizarUsuarioUnicoGet)
+router.get('/VisualizarDadosGeral', estaAutenticado, new UsuariosControllers().visualizarDadosGeral)
+router.delete('/ApagarUsuarios', estaAutenticado, new UsuariosControllers().apagarUsuarios)
 router.post('/LoginUsuarios', new LoginUsuariosControllers().logarUsuarios)
 
 //Rotas de Cargos
-router.post('/CadastrarCargos', new CargosControllers().cadastrarCargos)
+router.post('/CadastrarCargos', estaAutenticado, new CargosControllers().cadastrarCargos)
 
 //Rotas de Produtos
-router.post('/CadastrarProdutos', upload.single('file'), new ProdutosControllers().cadastrarProdutos)
-router.get('/VisualizarProdutos', new ProdutosControllers().visualizarProdutosGeral)
+router.post('/CadastrarProdutos', estaAutenticado, upload.single('file'), new ProdutosControllers().cadastrarProdutos)
+router.get('/VisualizarProdutos', estaAutenticado, new ProdutosControllers().visualizarProdutosGeral)
 
 
 export default router
